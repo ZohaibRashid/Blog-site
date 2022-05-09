0e80f4637e1ae971160e06d1392a0d57c4804c36
Zohaib R <zohaibr123@hotmail.com>
2022-05-09 05:59:25 -0400

    add routing parameter to display each post on separate page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,11 +16,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let postTitle = [];
-let postBody = [];
+let postArray= [];
+
 app.get("/", function(req, res){
 
-  res.render("home", {content: homeStartingContent, title: postTitle, body: postBody});
+  res.render("home", {content: homeStartingContent, postsList: postArray});
 
 });
 
@@ -45,19 +46,44 @@ app.get("/compose", function(req, res){
 });
 
 
+
 app.post("/compose", function(req, res){
 
-   postTitle.push(req.body.title);
-   postBody.push(req.body.content); 
+  let posts = {
+     title : req.body.title,
+     content: req.body.content
+  };
 
-   res.redirect("/");
  
+  postArray.push(posts);
+ 
+  res.redirect("/");
+
 
 });
 
+app.get("/posts/:newPost", function(req,res){
+
+  const requestedTitle = req.params.newPost;
+  //console.log(requestedTitle);
+  var found = 0;
+  let i = 0;
+for(i = 0; i< postArray.length; i++){
+  if(_.lowerCase(requestedTitle) === _.lowerCase(postArray[i].title)){
+    found = 1;
+    break;
+  }
+}
+  if(found){
+
+    res.render("post", {title:_.capitalize(requestedTitle), body:postArray[i].content});
+  }
+  else{
+   console.log("No Match found!!"); 
+  }
 
 
-
+});
 
 
 app.listen(3000, function() {
